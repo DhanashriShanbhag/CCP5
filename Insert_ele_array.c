33967732f6adb5009771f2bb26157490196d0b7b
DhanashriShanbhag <84217862+DhanashriShanbhag@users.noreply.github.com>
2021-06-19 09:44:08 +0530

    Update Insert_ele_array.c

--- Insert_ele_array.c
+++ Insert_ele_array.c
@@ -5,39 +5,30 @@
 
 int main()
 {
-    int arr[100] = { 0 };
-    int i, x, pos, n = 10;
- 
-    // initial array of size 10
-    for (i = 0; i < 10; i++)
-        arr[i] = i + 1;
- 
-    // print the original array
-    for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
- 
-    // element to be inserted
-    x = 50;
- 
-    // position at which element
-    // is to be inserted
-    pos = 5;
- 
-    // increase the size by 1
-    n++;
- 
-    // shift elements forward
-    for (i = n-1; i >= pos; i--)
-        arr[i] = arr[i - 1];
- 
-    // insert x at pos
-    arr[pos - 1] = x;
- 
-    // print the updated array
-    for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
- 
-    return 0;
+   int array[100], position, c, n, value;
+   printf("Enter number of elements in array\n");
+   scanf("%d", &n);
+
+   printf("Enter %d elements\n", n);
+
+   for (c = 0; c < n; c++)
+      scanf("%d", &array[c]);
+
+   printf("Enter the location where you wish to insert an element\n");
+   scanf("%d", &position);
+
+   printf("Enter the value to insert\n");
+   scanf("%d", &value);
+
+   for (c = n - 1; c >= position - 1; c--)
+      array[c+1] = array[c];
+
+   array[position-1] = value;
+
+   printf("Resultant array is\n");
+
+   for (c = 0; c <= n; c++)
+      printf("%d\n", array[c]);
+
+   return 0;
 }
